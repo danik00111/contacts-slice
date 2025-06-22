5e49614f9a7de69ee457416dcd992d3b820dbd6c
danik00111 <kremenskiydaniil@gmail.com>
2025-06-22 19:11:29 +0300

    idk anymor

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { store } from './redux/store'
+import { store, persistor } from './redux/store'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <App/>
+      {/* <PersistGate store={persistor}> */}
+        <App/>
+      {/* </PersistGate> */}
     </Provider>
   </StrictMode>
 )
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,25 @@
+import { persistStore, persistReducer, FLUSH,
+  REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from "redux-persist";
 import { contactReducer, filterReducer } from './reducers'
 import { configureStore } from '@reduxjs/toolkit'
+import storage from "redux-persist/lib/storage"; // localStorage?
+
+const persistConfig = {
+  key: "root", // кореневий ключ для збереження даних
+  storage,
+};
+const persistedContactReducer = persistReducer(persistConfig, contactReducer);
 
 export const store = configureStore({
   reducer: {
-    cont: contactReducer,
+    cont: persistedContactReducer,
     fil: filterReducer,
   },
+  middleware: (getDefMW) => getDefMW({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
 });
+
+export const persistor = persistStore(store);
